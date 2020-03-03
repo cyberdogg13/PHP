5079703d77e806082fa524af5a29a1d94439659c
cyberdogg13 <timvanderstam@hotmail.com>
2020-03-03 11:42:24 +0100

    hoofdstuk 5 opdracht 5 bezig

--- Hoofdstuk5/loginformulier.php
+++ Hoofdstuk5/loginformulier.php
@@ -1,8 +1,5 @@
 <?php
 
-print_r($_POST);
-echo $_POST["E-Mail"];
-
 $EMails[0] = "piet@worldonline.nl";
 $EMails[1] = "klaas@carpets.nl";
 $EMails[2] = "truushendriks@wegweg.nl";
@@ -10,5 +7,15 @@ $wachtwoorden[0] = "doetje123";
 $wachtwoorden[1] = "snoepje777";
 $wachtwoorden[2] = "arkiearkie201";
 
+
+echo $_POST["E-Mail"] ;
+echo "<br>";
+echo $_POST["Wachtwoord"];
+echo "<br>";
+
+if ($_POST["E- Mail"] == "piet@worldonline.nl" && $_POST["Wachtwoord"] == "doetje123"){
+    echo "welkom";
+}
+else echo "niet welkom:" . $_POST["E-Mail"];
 
 ?>
