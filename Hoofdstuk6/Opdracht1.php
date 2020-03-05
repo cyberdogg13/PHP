3dc07df279482277e5e1469906423f98e70aa5b5
cyberdogg13 <timvanderstam@hotmail.com>
2020-03-05 14:18:36 +0100

    hoofdstuk 6 eindelijk verbinding met de sql server

--- Hoofdstuk6/Opdracht1.php
+++ Hoofdstuk6/Opdracht1.php
@@ -6,7 +6,7 @@ $pass = "";
 $db = "school";
 
 try {
-    $dbh = new PDO('mysql:host=localhost;port=3308;dbname=school', $user, $pass);
+    $dbh = new PDO('mysql:host=localhost;port='.$port.';dbname=school', $user, $pass);
     foreach($dbh->query("SELECT * from cursus") as $row) {
         print_r($row);
     }
