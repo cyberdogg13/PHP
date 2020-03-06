fd9783329fbebc34c7ab413c99544f364c3dce5a
cyberdogg13 <timvanderstam@hotmail.com>
2020-03-06 11:40:36 +0100

    hoofdstuk 6 opdracht 1 ietje verder

--- Hoofdstuk6/Opdracht1.php
+++ Hoofdstuk6/Opdracht1.php
@@ -4,15 +4,21 @@ $port = "3308";
 $user = "root";
 $pass = "";
 $db = "school";
-
+$i = 0;
 try {
     $dbh = new PDO('mysql:host=localhost;port='.$port.';dbname=school', $user, $pass);
-    foreach($dbh->query("SELECT * from cursus") as $row) {
-        print_r($row);
+    foreach($dbh->query("SELECT curs_code from cursus") as $row) {
+        foreach ($row as $ding){
+   $curscode[$i] = $ding;
+      $resultaat = array_unique($curscode, SORT_LOCALE_STRING);
+   print_r($resultaat);
+   echo "<br>";
+            $i++;
+        }
     }
-    $dbh = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
+
 ?>
