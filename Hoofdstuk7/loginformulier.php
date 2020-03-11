a3e704be4e22e0ef80c1e463d48c5d63aa7c0a12
tim van der stam <timvanderstam@hotmail.com>
2020-03-11 10:17:33 +0100

    begonnen aan hoofdstuk 7

--- Hoofdstuk7/loginformulier.php
+++ Hoofdstuk7/loginformulier.php
@@ -1,85 +1,53 @@
 <?php
 session_start();
 
+$users = array(
+    "janjaap" => array("wachtwoord" => "1234", "rol" => "admin"),
+    "bert" => array("wachtwoord" => "1235", "rol" => "gebruiker"),
+    "jans" => array("wachtwoord" => "123", "rol" => "admin")
+);
 
-$host = "localhost";
-$port = "3308";
-$user = "root";
-$pass = "";
-$db = "school";
-$dbh = new PDO("mysql:host=" . $host . ";dbname=" . $db . ";port=" . $port, $user, $pass);
-$correct = false;
-$j = 0;
-$k = 0;
-try {
-    foreach ($dbh->query('SELECT email from loginformulier') as $email) {
-        $EMails[$j] = $email["email"];
-        $j++;
-    }
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
 
-try {
-    foreach ($dbh->query('SELECT wachtwoord from loginformulier') as $wachtwoord) {
-        $wachtwoorden[$k] = $wachtwoord["wachtwoord"];
-        $k++;
-    }
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-try {
-    foreach ($dbh->query('SELECT email from admins') as $adminemail) {
-        $adminEMails[$j] = $adminemail["email"];
-        $j++;
-    }
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-
-try {
-    foreach ($dbh->query('SELECT wachtwoord from admins') as $adminwachtwoord) {
-        $adminwachtwoorden[$k] = $adminwachtwoord["wachtwoord"];
-        $k++;
-    }
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-
-if (isset($_GET["loguit"])){
-    $_SESSION =array();
+if (isset($_GET["loguit"])) {
+    $_SESSION = array();
     session_destroy();
-}
+};
+
+if (isset($_POST["knop"])
+&& isset($users[$_POST["login"]])
+&& $users[$_POST["login"]]["wachtwoord"] == $_POST["wachtwoord"]){
+    $_SESSION["user"] = $_POST["login"];
+    $bericht = "Welkom" . $_SESSION["user"];
+} else{
+    $bericht = "inloggen";
+};
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>login formulier</h1>
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-    E-Mail <input type= "email" name="E-Mail" value= "">
-    Wachtwoord <input type= "password" name="Wachtwoord" value= "">
-    <input type= submit name="knop" value= verstuur >
-    <br>
-    <a href="admin.php">de website</a>
-    <br>
-    <a href="loginformulier.php?loguit">uitloggen</a>
-</form>
-</body>
-</html>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    <h1>login formulier</h1>
+    <h2><?php echo $bericht; ?></h2>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+        Login <input type="text" name="login" value="">
+        Wachtwoord <input type="password" name="Wachtwoord" value="">
+        <input type=submit name="knop" value=verstuur>
+        <br>
+        <a href="admin.php">de website</a>
+        <br>
+        <a href="loginformulier.php?loguit">uitloggen</a>
+    </form>
+    </body>
+    </html>
 
 <?php
-if (isset($_POST["E-Mail"])) {
-    echo "<h1>welkom " . $_POST["E-Mail"] . " op deze website</h1>";
-}
+//if (isset($_POST["E-Mail"])) {
+//    echo "<h1>welkom " . $_POST["E-Mail"] . " op deze website</h1>";
+//}
 ?>
